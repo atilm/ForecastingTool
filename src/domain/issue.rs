ff8c6207beb8fbc61186ee4824540919078a5600
Andreas Thielmann <thielmann.andreas@googlemail.com>
2026-02-08 11:56:45 +0100

    issue status as enum

--- src/domain/issue.rs
+++ src/domain/issue.rs
@@ -1,12 +1,18 @@
-use std::collections::HashMap;
 use chrono::NaiveDate;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum IssueStatus {
+	ToDo,
+	InProgress,
+	Done,
+}
 
 #[derive(Debug, Clone, Default, PartialEq, Eq)]
 pub struct Issue {
 	pub issue_id: Option<String>,
 	pub title: Option<String>,
 	pub description: Option<String>,
-	pub status: Option<String>,
+	pub status: Option<IssueStatus>,
 	pub created_date: Option<NaiveDate>,
 	pub start_date: Option<NaiveDate>,
 	pub done_date: Option<NaiveDate>,
@@ -15,18 +21,6 @@ pub struct Issue {
 impl Issue {
 	pub fn new() -> Self {
 		Self::default()
-	}
-
-	pub fn to_dict(&self) -> HashMap<String, Option<String>> {
-		let mut map = HashMap::with_capacity(7);
-		map.insert("issue_id".to_string(), self.issue_id.clone());
-		map.insert("title".to_string(), self.title.clone());
-		map.insert("description".to_string(), self.description.clone());
-		map.insert("status".to_string(), self.status.clone());
-		map.insert("created_date".to_string(), self.created_date.map(|d| d.to_string()));
-		map.insert("start_date".to_string(), self.start_date.map(|d| d.to_string()));
-		map.insert("done_date".to_string(), self.done_date.map(|d| d.to_string()));
-		map
 	}
 }
 
@@ -44,28 +38,6 @@ mod tests {
 		assert_eq!(issue.created_date, None);
 		assert_eq!(issue.start_date, None);
 		assert_eq!(issue.done_date, None);
-	}
-
-	#[test]
-	fn to_dict_contains_all_keys_and_values() {
-		let issue = Issue {
-			issue_id: Some("ID-123".into()),
-			title: Some("A title".into()),
-			description: None,
-			status: Some("Done".into()),
-			created_date: Some(NaiveDate::from_ymd_opt(2025, 1, 1).unwrap()),
-			start_date: None,
-			done_date: Some(NaiveDate::from_ymd_opt(2025, 2, 1).unwrap()),
-		};
-
-		let dict = issue.to_dict();
-		assert_eq!(dict.get("issue_id").cloned().flatten(), Some("ID-123".into()));
-		assert_eq!(dict.get("title").cloned().flatten(), Some("A title".into()));
-		assert_eq!(dict.get("description").cloned().flatten(), None);
-		assert_eq!(dict.get("status").cloned().flatten(), Some("Done".into()));
-		assert_eq!(dict.get("created_date").cloned().flatten(), Some("2025-01-01".into()));
-		assert_eq!(dict.get("start_date").cloned().flatten(), None);
-		assert_eq!(dict.get("done_date").cloned().flatten(), Some("2025-02-01".into()));
 	}
 }
 
