b56f925885a06267d8f5ec88a0a81184eae23099
Andreas Thielmann <thielmann.andreas@googlemail.com>
2026-07-03 20:29:21 +0200

    trigger build script every time

--- build.rs
+++ build.rs
@@ -1,11 +1,27 @@
+use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
+use std::time::{SystemTime, UNIX_EPOCH};
+
 fn main() {
-    // note: add error checking yourself.
     let output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rerun-if-changed=.git/HEAD");
+
+    let rerun_trigger = rerun_trigger_path();
+    let timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_nanos()
+        .to_string();
+    fs::write(&rerun_trigger, timestamp).unwrap();
+
+    println!("cargo:rerun-if-changed={}", rerun_trigger.display());
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+}
+
+fn rerun_trigger_path() -> PathBuf {
+    PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("git-hash-rerun-trigger")
 }
